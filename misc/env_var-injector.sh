ae01395c4174c6925b82eca9b2c47d2589058e79
benka <andrew.benkoczi@gmail.com>
2017-01-26 19:45:31 -0500

    env inj. fix

--- misc/env_var-injector.sh
+++ misc/env_var-injector.sh
@@ -14,10 +14,9 @@ if [ ! -e $target ] ; then
   echo "#target: created target file [$target]..."
 fi
 
-
 echo "#env_var_array: [$env_var_array]"
 for i in ${env_var_array[@]} ; do
-  echo export $i=$(eval echo \$$i) | tee test.log
   add_env_var="export $i=$(eval echo \$$i)"
-  sed -i "1s/^/export $add_env_var\n/" $target
+  echo $add_env_var
+  sed -i "1s|^|$add_env_var\n|" $target
 done
