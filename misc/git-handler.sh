d0cb35d920d5dfe6d98149b441ccf39a7bc2583e
benka <andrew.benkoczi@gmail.com>
2017-03-06 16:02:35 -0500

    git-handler adding git remote feature

--- misc/git-handler.sh
+++ misc/git-handler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 echo ""
-echo "### git-handler v1.2 ###"
+echo "### git-handler v1.3 ###"
 echo ""
 for i in "$@"
 do
@@ -21,9 +21,17 @@ do
 	    -h=*|--homedir=*)
 	    homedir="${i#*=}"
 	    shift # past argument=value
-	    ;;	    
+	    ;;
+	    --remote_name=*)
+	    remote_name="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	    --remote_url=*)
+	    remote_url="${i#*=}"
+	    shift # past argument=value
+	    ;;
 	    *)
-	            # unknown option
+	    	# unknown option
 	    ;;
 	esac
 done
@@ -36,6 +44,8 @@ if [ "$repo" == "" ] ; then
 	echo " -d, --dir [optional]: directory to clone git repository"
 	echo " -b, --branch [optional, default: master]: branch to check out (and pull from)"
 	echo " -h, --homedir [optional, default: .]: home path to home /or working directory"
+	echo " --remote_name [optional, default: none] when used, script add the specified remote_name and remote_url"
+	echo " --remote_url [optional, default: none] use with remote_name"
 	echo ""
 	exit 1
 fi
@@ -45,8 +55,6 @@ fi
 if [ "$homedir" == "" ] ; then
 	homedir="."
 fi
-cd $homedir
-pwd
 
 # Checking if branch was provided, 
 # otherwise setting it to default: master
@@ -60,25 +68,47 @@ if [ "$dir" == "" ] ; then
   echo "dir: $dir"
 fi 
 
+if [ "$remote_name" == "" ] ; then
+	remote_url=""
+elif [ "$remote_name" != "" ] && [ "$remote_url" == "" ] ; then
+	remote_name=""
+fi
 
+echo "---------------------------"
 echo "REPO: $repo"
 echo "DIR: $dir"
 echo "HOMEDIR: $homedir"
 echo "BRANCH: $branch"
+echo "REMOTE_NAME: $remote_name"
+echo "REMOTE_URL: $remote_url"
+echo "---------------------------"
 
+# Changing to HOMEDIR
+mkdir -p $homedir && cd $_
 
 # Cloning GIT repo
+echo "currently in dir $(pwd)"
+echo "git clone $repo $dir"
 git clone $repo $dir > /dev/null 2>&1
-success=$?
+clone_success=$?
 
-if [ "$success" != "0" ] ; then
+if [ "$clone_success" != "0" ] ; then
   echo "Repository is already cloned"
 fi
 
-
 # Enter repo dir
 cd $dir
+
 # Checkout and pull branch
 git checkout $branch
 git pull origin $branch
 
+if [ "$remote_name" != "" ] ; then
+	# Remove and Create git-remote
+	git remote remove $remote_name > /dev/null 2>&1
+	git remote add $remote_name $remote_url
+	remote_success=$?
+	if [ "$remote_success" != "0" ] ; then
+		echo "Error with creating git remote"
+	fi
+fi
